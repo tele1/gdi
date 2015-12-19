a46ddbe2d209d63984660769a9928284b0c08d76
gg <gg@localhost.localdomain>
2015-12-19 14:30:26 +0100

    new block

--- data/list/sort.sh
+++ data/list/sort.sh
@@ -1,1 +1,24 @@
 #!/bin/sh
+
+
+ a=$(ls | grep ".list")
+ b=$(echo "$a" | wc -l)
+
+
+echo "$a" > /tmp/sort.gdi
+echo "created /tmp/sort.gdi"
+echo "$b"
+echo "========"
+
+		for i in `seq 1 $b`
+		do
+
+
+			#line from file
+			c=$(awk 'NR=='$i /tmp/sort.gdi)
+			echo "$c"
+
+			echo "$(cat "$c" | sort)" > "$c"
+		done
+
+rm  -vif /tmp/sort.gdi
